c8d3b2b5d52e1235f3349cfea80638094004ba97
gk100799 <55948183+gk100799@users.noreply.github.com>
2020-10-05 15:33:44 +0530

    Updates

---
+++ precache-manifest.4576a7b0f1f0e794abc9d67e280c85ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08507bed497e3ea4e5e43949aa1ed395",
+    "url": "/ers_reactjs/index.html"
+  },
+  {
+    "revision": "ff08d5ebe40f5c4afcb4",
+    "url": "/ers_reactjs/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "4e1e0a4950b62a6688e3",
+    "url": "/ers_reactjs/static/css/main.3ce14239.chunk.css"
+  },
+  {
+    "revision": "ff08d5ebe40f5c4afcb4",
+    "url": "/ers_reactjs/static/js/2.35fcd9f7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ers_reactjs/static/js/2.35fcd9f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1e0a4950b62a6688e3",
+    "url": "/ers_reactjs/static/js/main.15ab59bc.chunk.js"
+  },
+  {
+    "revision": "8cf1f4ae2ccc19f277ea",
+    "url": "/ers_reactjs/static/js/runtime-main.5eafe757.js"
+  },
+  {
+    "revision": "3df437922930cf2e2cbbe9f5b22132d3",
+    "url": "/ers_reactjs/static/media/bg-stud.3df43792.jpg"
+  }
+]);
